5efeb7263d5631572e8a409cffabe056f182cb1b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-04-14 06:02:05 +0700

    Move to Root

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 // Stream TSV or CSV file contains list of URL redirection data
 if (
-    is_file($f = LOT . DS . 'page' . DS . 'kick.csv') ||
-    is_file($f = LOT . DS . 'page' . DS . 'kick.tsv')
+    is_file($file = LOT . D . 'page' . D . 'kick.csv') ||
+    is_file($file = LOT . D . 'page' . D . 'kick.tsv')
 ) {
     // Get current URL path to be redirected
-    $path = $url->path . $url->i . $url->query . $url->hash;
+    $path = $url->path . $url->query . $url->hash;
     // Stream now!
-    if (false !== ($h = fopen($f, 'r'))) {
-        $csv = 'csv' === pathinfo($f, PATHINFO_EXTENSION);
+    if (false !== ($h = fopen($file, 'r'))) {
+        $csv = 'csv' === pathinfo($file, PATHINFO_EXTENSION);
         while (false !== ($v = fgetcsv($h, 1024, $csv ? ',' : "\t"))) {
             if ($path === $v[0] && !empty($v[1])) {
                 fclose($h);
-                header('location: ' . URL::long($v[1], false));
+                header('location: ' . long($v[1]));
                 exit;
             }
         }
